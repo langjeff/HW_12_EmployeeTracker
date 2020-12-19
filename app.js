9dd019bac77a44af76ef6a3fef4ce82cdee6c64c
IronJon <lang.jeffrey@gmail.com>
2020-12-18 20:22:00 -0700

    set up connection to server in app.js

--- app.js
+++ app.js
@@ -7,3 +7,43 @@
 // Add Role -- INSERT INTO
 // View All Departments -- SELECT Left Join
 // Add Department - INSERT INTO
+
+//dependencies
+const express = require("express");
+const mysql = require("mysql");
+const inquirer = require("inquirer");
+
+const app = express();
+
+// Set the port of our application
+// process.env.PORT lets the port be set by Heroku
+const PORT = process.env.PORT || 8080;
+
+// Sets up the Express app to handle data parsing
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+//Sets up db connection
+const connection = mysql.createConnection({
+  host: "localhost",
+  port: 3306,
+  user: "root",
+  password: "@uth3nt1c",
+  database: "company_db",
+});
+
+connection.connect((err) => {
+  if (err) {
+    console.error(`error connecting: ${err.stack}`);
+    return;
+  }
+
+  console.log(`connected as id ${connection.threadId}`);
+});
+
+
+// Start our server so that it can begin listening to client requests.
+// Log (server-side) when our server has started
+app.listen(PORT, () =>
+  console.log(`Server listening on: http://localhost:${PORT}`)
+);
